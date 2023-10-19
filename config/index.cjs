871d7915741de7123f4bacc6c97df1aad6ae2082
DAENINE <873930952@qq.com>
2023-10-19 19:49:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,13 +15,13 @@ USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '2dd1d8f07e8466b2014df368d76399b8',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '广西',
+  CITY: '桂林',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '梁敏宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'e032e970309c40d19246c85a5a17ae6d',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -31,20 +31,20 @@ USE_PASSAGE: 'push-plus',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '梁敏', year: '1997', date: '07-25',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '梁敏', year: '1997', date: '08-27',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '08-30',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-08-30' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
